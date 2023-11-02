86dab0588399305c74eef7f1217b979389c1595b
JamieCattoCode <jamietcatto@gmail.com>
2023-11-02 10:31:08 +0100

    aider: The code has been updated to parse the command line argument for the prompt, connect to the database, get the table definitions, update the prompt with the table definitions and database URL, get a response from the AI, parse the response into SQL statements, and run each SQL statement.
    # Aider chat conversation:
    
    USER: Read the comments in the main() function. Generate code based on the comment. Read db.py and llm.py to see what funcitonality you have available. Only update the main function.
    
    ASSISTANT: To generate the code based on the comments in the `main()` function, we will need to modify the `postgres_ai_agent/main.py` file.
    
    Here are the steps we will follow:
    
    1. Import the necessary libraries and modules.
    2. Parse the prompt parameter using `argparse`.
    3. Connect to the database and create a `db_manager`.
    4. Get table definitions for the prompt.
    5. Create two blank calls to `llm.add_cap_ref()` that update our current prompt passed in from the command line.
    6. Call `llm.prompt` to get a `prompt_response` variable.
    7. Parse SQL response from `prompt_response` using SQL_QUERY_DELIMITER '---------'.
    8. Call `db_manager.run_sql()` with the parsed SQL.
    
    Here is the *SEARCH/REPLACE block* for the changes:
    
    ```python
    postgres_ai_agent/main.py
    <<<<<<< SEARCH
    def main():
        # parse prompt param using arg parse
    
        # connect to db with statement and create a db_manager
        with PostgresDB() as db:
            db.connect_with_url(DB_URL)
    
            users_table = db.get_all('users')
    
            print("Users table", users_table)
    
        # call db_manager.get_table_definitions_for_prompt() to get tables in prompt ready form
    
        # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
    
        # call llm.prompt to get a prompt_response variable
    
        # parse SQL response from prompt_response using SQL_QUERY_DELIMITER '---------'
    
        # call db_manager.run_sql() with the parsed sql
    
    if __name__ == '__main__':
        main()
    =======
    import argparse
    
    def main():
        # parse prompt param using arg parse
        parser = argparse.ArgumentParser()
        parser.add_argument("prompt", help="Prompt for the AI")
        args = parser.parse_args()
    
        # connect to db with statement and create a db_manager
        with PostgresDB() as db:
            db.connect_with_url(DB_URL)
    
            # call db_manager.get_table_definitions_for_prompt() to get tables in prompt ready form
            table_definitions = db.get_table_definitions_for_prompt()
    
            # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
            prompt = llm.add_cap_ref(args.prompt, "", "TABLE_DEFINITIONS", table_definitions)
            prompt = llm.add_cap_ref(prompt, "", "DATABASE_URL", DB_URL)
    
            # call llm.prompt to get a prompt_response variable
            prompt_response = llm.prompt(prompt)
    
            # parse SQL response from prompt_response using SQL_QUERY_DELIMITER '---------'
            sql_statements = prompt_response.split('---------')
    
            # call db_manager.run_sql() with the parsed sql
            for sql in sql_statements:
                db.run_sql(sql)
    
    if __name__ == '__main__':
        main()
    >>>>>>> REPLACE
    ```
    
    This code will parse the command line argument for the prompt, connect to the database, get the table definitions, update the prompt with the table definitions and database URL, get a response from the AI, parse the response into SQL statements, and run each SQL statement.

--- postgres_ai_agent/main.py
+++ postgres_ai_agent/main.py
@@ -11,26 +11,34 @@ assert os.environ.get('OPEN_AI_API_KEY'), "OPEN_AI_API_KEY not found in .env fil
 DB_URL = os.environ.get('DATABASE_URL')
 OPEN_AI_API_KEY = os.environ.get('OPEN_AI_API_KEY')
 
+import argparse
+
 def main():
     # parse prompt param using arg parse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("prompt", help="Prompt for the AI")
+    args = parser.parse_args()
 
     # connect to db with statement and create a db_manager
     with PostgresDB() as db:
         db.connect_with_url(DB_URL)
 
-        users_table = db.get_all('users')
+        # call db_manager.get_table_definitions_for_prompt() to get tables in prompt ready form
+        table_definitions = db.get_table_definitions_for_prompt()
 
-        print("Users table", users_table)
-    
-    # call db_manager.get_table_definitions_for_prompt() to get tables in prompt ready form
+        # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
+        prompt = llm.add_cap_ref(args.prompt, "", "TABLE_DEFINITIONS", table_definitions)
+        prompt = llm.add_cap_ref(prompt, "", "DATABASE_URL", DB_URL)
 
-    # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
+        # call llm.prompt to get a prompt_response variable
+        prompt_response = llm.prompt(prompt)
 
-    # call llm.prompt to get a prompt_response variable
+        # parse SQL response from prompt_response using SQL_QUERY_DELIMITER '---------'
+        sql_statements = prompt_response.split('---------')
 
-    # parse SQL response from prompt_response using SQL_QUERY_DELIMITER '---------'
-
-    # call db_manager.run_sql() with the parsed sql
+        # call db_manager.run_sql() with the parsed sql
+        for sql in sql_statements:
+            db.run_sql(sql)
 
 if __name__ == '__main__':
     main()
